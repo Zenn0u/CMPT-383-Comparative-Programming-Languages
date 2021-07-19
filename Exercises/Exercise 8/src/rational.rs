6d8e5e444eedd88af4754371aa1d4a2ba1a8b662
Alp <tbaltaci@sfu.ca>
2021-07-19 08:53:54 -0700

    Finished Exercise 8

---
+++ Exercises/Exercise 8/src/rational.rs
@@ -0,0 +1,40 @@
+use std::fmt;
+
+fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        return a;
+    }
+    else {
+        return gcd(b, a % b)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Rational {
+    pub n: i64,
+    pub d: i64,
+}
+
+impl Rational {
+    pub fn new(n: i64, d: i64) -> Rational {
+        Rational {
+            n: n,
+            d: d,
+        }
+    }
+
+    pub fn reduce(&mut self) {
+        let x = gcd(self.n, self.d);
+        self.n /= x;
+        self.d /= x;
+    }
+}
+
+impl From<i64> for Rational {
+    fn from(n: i64) -> Rational {
+        Rational {
+            n: n,
+            d: 1,
+        }
+    }
+}
